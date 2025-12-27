e5cfefb73cefa2ff97d78ecc5dc8f583466feb44
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-12-27 03:09:40 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,53 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
+import ProtectedRoute from './routes/ProtectedRoute.jsx';
 import Header from './components/Header.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import ScorePage from './pages/ScorePage.jsx';
 import SpectatorPage from './pages/SpectatorPage.jsx';
 import AdminPage from './pages/AdminPage.jsx';
-import ProtectedRoute from './routes/ProtectedRoute.jsx';
 import Countdown from './pages/Countdown.jsx';
+
 function App() {
   return (
     <AuthProvider>
-      <HashRouter basename='/PadelScoreLiveFront/'>
+      <BrowserRouter basename='/PadelScoreLiveFront/' >
         <div className="min-h-screen flex flex-col">
           <Header />
           <main className="flex-1">
             <Routes>
+                                            <Route path="/count" element={<Countdown />} />
+
               <Route path="/login" element={<LoginPage />} />
-              <Route path="/" element={<ProtectedRoute><ScorePage /></ProtectedRoute>} />
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <ScorePage />
+                  </ProtectedRoute>
+                }
+              />
               <Route path="/spectator" element={<SpectatorPage />} />
-              <Route path="/admin" element={<ProtectedRoute><AdminPage /></ProtectedRoute>} />
-              <Route path="/count" element={<Countdown />} />
+              <Route
+                path="/admin"
+                element={
+                  <ProtectedRoute>
+                    <AdminPage />
+                  </ProtectedRoute>
+                }
+              />
               <Route path="*" element={<Navigate to="/" replace />} />
+                              <Route path="/count" element={<Countdown />} />
+
             </Routes>
           </main>
         </div>
-      </HashRouter>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
+
 export default App;
+
+
