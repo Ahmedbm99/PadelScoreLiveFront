ba36f2fcfcccb652d43f85efca2e156e80896d3f
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-12-27 03:03:13 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext.jsx';
+import Header from './components/Header.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import ScorePage from './pages/ScorePage.jsx';
+import SpectatorPage from './pages/SpectatorPage.jsx';
+import AdminPage from './pages/AdminPage.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import Countdown from './pages/Countdown.jsx';
 
 function App() {
   return (
